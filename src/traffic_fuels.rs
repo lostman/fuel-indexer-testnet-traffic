6295b75729bd2cff02135b20fafd0ba9e103e99e
Maciej Woś <maciej.wos@gmail.com>
2023-12-05 14:36:12 +0000

    update

--- src/traffic_fuels.rs
+++ src/traffic_fuels.rs
@@ -1,5 +1,4 @@
 use fuels::prelude::*;
-use fuels::programs::call_response::FuelCallResponse;
 
 fuels::prelude::abigen!(Contract(
     name = "Subcurrency",
@@ -8,76 +7,71 @@ fuels::prelude::abigen!(Contract(
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Launching node and creating wallets");
+    println!("> Launching node and creating wallets");
 
-    let wallet_config = WalletsConfig::new(Some(10), None, None);
+    let wallet_config = WalletsConfig::new(Some(10), Some(10), None);
     let mut provider_config = Config::default();
     provider_config.block_production = Trigger::Never;
     let wallets =
         launch_custom_provider_and_get_wallets(wallet_config, Some(provider_config), None).await?;
     let mint_wallet = wallets[0].clone();
-    let wallets = wallets[1..].to_vec();
 
     let node = mint_wallet.provider().unwrap().clone();
 
     let configurables = SubcurrencyConfigurables::new().with_MINTER(mint_wallet.address().into());
 
-    // Produce 1 block / 5 seconds
-    tokio::task::spawn({
-        let node = node.clone();
-        async move {
-            loop {
-                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-                let height = node.produce_blocks(1, None).await.unwrap();
-                println!("Block #{height}");
-            }
-        }
-    });
-
-    println!("Deploying contract");
+    println!("> Deploying contract");
 
     let contract_id = Contract::load_from(
         "contracts/subcurrency/out/debug/subcurrency.bin",
         LoadConfiguration::default().with_configurables(configurables),
     )?
-    .deploy(&mint_wallet, TxPolicies::default())
-    .await?;
+    .deploy(&mint_wallet, TxPolicies::default());
+
+    let block = async {
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        node.produce_blocks(1, None).await.unwrap();
+    };
+
+    let (contract_id, _) = tokio::join!(contract_id, block);
+    let contract_id = contract_id.unwrap();
 
     let contract_instance = Subcurrency::new(contract_id.clone(), mint_wallet.clone());
 
     println!(
-        "Block height: {}",
+        "> Block height: {}",
         node.latest_block_height().await.unwrap()
     );
 
-    println!("Minting coins");
+    println!("> Minting coins");
 
-    contract_instance
-        .methods()
-        .mint(mint_wallet.address(), 300)
-        .call()
-        .await
-        .unwrap();
-
-    println!("Minting more coins...");
-
+    let mut results = vec![];
     for i in 0..wallets.len() {
         println!("Minting 100 coins to {}", wallets[i].address());
-        contract_instance
+        let resp = contract_instance
             .methods()
             .mint(wallets[i].address(), 100)
-            .call()
+            .submit()
             .await
             .unwrap();
+        results.push(resp)
+    }
+
+    node.produce_blocks(1, None).await.unwrap();
+
+    // Check the responses
+    for r in results {
+        r.response().await.unwrap();
     }
 
     println!(
-        "Block height: {}",
+        "> Block height: {}",
         node.latest_block_height().await.unwrap()
     );
 
-    println!("Generating transfers");
+    println!("> Generating transfers");
 
+    let mut results = vec![];
     for w in wallets.iter() {
         let contract_instance = Subcurrency::new(contract_id.clone(), w.clone());
         println!(
@@ -85,36 +79,46 @@ async fn main() -> anyhow::Result<()> {
             w.address(),
             mint_wallet.address()
         );
-        contract_instance
+        let resp = contract_instance
             .methods()
             .send(mint_wallet.address(), 3)
-            .call()
+            .submit()
             .await
             .unwrap();
+        results.push(resp);
     }
 
-    println!("Checking balances");
+    node.produce_blocks(1, None).await.unwrap();
+
+    // Check the responses
+    for r in results {
+        r.response().await.unwrap();
+    }
+
+    println!(
+        "> Block height: {}",
+        node.latest_block_height().await.unwrap()
+    );
+
+    println!("> Checking balances");
 
     for w in wallets.iter() {
         let contract_instance = Subcurrency::new(contract_id.clone(), w.clone());
-        let balance: FuelCallResponse<u64> = contract_instance
+        let resp = contract_instance
             .methods()
             .balance(w.address())
-            .call()
+            .simulate()
             .await
             .unwrap();
-        println!("Balance {} {}", w.address(), balance.value);
+        println!("Balance {} {}", w.address(), resp.value);
     }
-
-    // let r: FuelCallResponse<((), ())> = multi_call_handler.call().await.unwrap();
-    // println!("{:#?}", r.receipts);
 
     let chain_info = node.chain_info().await.unwrap();
 
-    println!("Chain Info:");
-    println!("{chain_info:#?}");
+    println!("> Latest Block:");
+    println!("{:#?}", chain_info.latest_block);
 
-    println!("Done.");
+    println!("X Done.");
 
     Ok(())
 }
